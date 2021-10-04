4d930dafa1be4bbeee3f6ed9168dd86460c19f60
Chandan Kumar <chandanchandu287@gmail.com>
2021-10-04 17:26:10 +0530

    Solve block4 exercise of Decision and Loops

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,11 +3,34 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
+for(let i = 0; i <=10; i = i + 1) {
+    console.log(i);
+}
 
-// Using `console.log` log all the evan values from 1 to 10.
-
-// Using `console.log` log all the od values from 1 to 10.
+// Using `console.log` log all the even values from 1 to 10.
+for(let i = 0; i <=10; i = i + 1) {
+    if(i % 2 == 0) {
+    console.log(i);
+    }
+}
+// Using `console.log` log all the odd values from 1 to 10.
+for(let i = 0; i <=10; i = i + 1) {
+    if(i % 2 !== 0) {
+    console.log(i);
+    }
+}
 
 // Calculate the sum of all numbers from 1 to 10.
+for(let i = 0; i <=10; i = i + 1) {
+    if(i ) {
+    console.log(i);
+    }
+}
 
 // Log all the values from 1 to 10 using while loop
+let i = 0;
+while(i < 10) {
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
